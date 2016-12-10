e74971f4a4af31ee50e97baf2ec6d54749240984
Ana Đorđević <4na@tuta.io>
2016-12-10 17:29:53 +1000

    helper method !

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  private
-
   def site_config
     @site_config ||= Rails.application.config.site_config
   end
+  helper_method :site_config
 end
