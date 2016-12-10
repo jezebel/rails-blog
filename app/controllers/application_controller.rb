f032b54ff272a23373647fe30f2f48f2c272c9f6
Ana Đorđević <4na@tuta.io>
2016-12-10 17:06:15 +1000

    site config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def site_config
+    @site_config ||= Rails.application.config.site_config
+  end
 end
