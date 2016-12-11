82305e2f32436c6ecfa18d9ed0e678994905bec3
Ana Đorđević <4na@tuta.io>
2016-12-11 11:26:08 +1000

    update site config

--- config/initializers/site_config.rb
+++ config/initializers/site_config.rb
@@ -1,22 +1,8 @@
-class SiteConfig
-  def initialize
-    config = YAML.load_file(Rails.root.join('config', "config.yml"))
-    process_hash(config)
-    self
-  end
-
-  private
-
-  def process_hash(config_hash)
-    config_hash.each do |key, value|
-      if value.is_a? Hash
-        process_hash(value)
-      else
-        self.class.send(:attr_accessor, key.to_sym)
-        instance_variable_set("@#{key}", value)
-      end
-    end
+module SiteConfig
+  def self.call
+    _config = YAML.load_file(Rails.root.join('config', "config.yml"))
+    Rails.application.config.site_config = OpenStruct.new(_config)
   end
 end
 
-Rails.application.config.site_config = SiteConfig.new
+SiteConfig.call
