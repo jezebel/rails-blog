2498e8ca360bc35cd8c6f7c23b17e6f1ce277ede
Ana Đorđević <4na@tuta.io>
2016-12-12 12:13:53 +1000

    give it a name so we can use path helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
 
-  get '/:id', to: 'pages#show'
+  get '/:id', to: 'pages#show', as: 'page'
 
   get '/', to: 'posts#index'
 end
