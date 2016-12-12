f831cc331346522aa629fd05307ca9b226ca9f9d
Ana Đorđević <4na@tuta.io>
2016-12-12 12:38:35 +1000

    blog model images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,17 @@ module ApplicationHelper
   def svg_path file_path
     File.read(file_path).html_safe if File.exists?(file_path)
   end
+
+  def blog_model_image blog_model
+    image   = blog_model.image
+    caption = blog_model.caption
+
+    content_tag :figure, class: 'post-image' do
+      image_tag image, alt: caption do
+        content_tag :figcaption do
+          "-- #{caption}"
+        end
+      end
+    end
+  end
 end
