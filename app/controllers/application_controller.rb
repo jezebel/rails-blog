5a10fe5654a8c79c4a49bc7491498cce30ee870a
Ana Đorđević <4na@tuta.io>
2016-12-12 17:25:34 +1000

    use site_config or init new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def site
-    @site ||= Site.new
+    @site ||= Rails.application.config.site_config || Site.new
   end
   helper_method :site
 end
