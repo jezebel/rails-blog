4244040cf59e767997234722954539cd8d16c8ee
Ana Đorđević <4na@tuta.io>
2016-12-12 12:16:28 +1000

    svg icon helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def svg_icon_path name
+    file_path = "#{Rails.root}/app/assets/images/icons/icon-#{name}.svg"
+    svg_path file_path
+  end
+
+  def svg_path file_path
+    File.read(file_path).html_safe if File.exists?(file_path)
+  end
 end
