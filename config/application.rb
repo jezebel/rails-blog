19ff6574ec2598108fb6faa4618bc2e87633a85c
Ana Đorđević <4na@tuta.io>
2016-12-17 20:45:47 +1000

    include more things

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,21 @@
 require_relative 'boot'
 
-# require 'rails/all'
-require "action_controller/railtie"
-require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'rails'
+
+%w(
+  action_controller/railtie
+  action_view/railtie
+  active_job/railtie
+  active_model/railtie
+  action_cable/engine
+  rails/test_unit/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
