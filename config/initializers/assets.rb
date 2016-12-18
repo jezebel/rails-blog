fdd28ec49435769d32b447c73218b19af9123a18
Ana Đorđević <4na@tuta.io>
2016-12-18 17:10:59 +1000

    fix asset paths

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,10 +4,10 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-Rails.application.config.assets.paths += Dir.glob("pages/*/")
-Rails.application.config.assets.paths += Dir.glob("posts/*/*/*/")
+Rails.application.config.assets.paths += Dir.glob("pages/*/").map{|x| File.join Rails.root, x}
+Rails.application.config.assets.paths += Dir.glob("posts/*/*/*/").map{|x| File.join Rails.root, x}
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += Dir.glob("pages/*/*.{jpg,png}").map{|x| File.basename(x)}
-Rails.application.config.assets.precompile += Dir.glob("posts/*/*/*/*.{jpg.png}").map{|x| File.basename(x)}
+Rails.application.config.assets.precompile += Dir.glob("posts/*/*/*/*.{jpg,png}").map{|x| File.basename(x)}
